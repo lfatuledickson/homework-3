98e5c6c5e4c59fa4a28f856b7fb38e3fa6d3019c
lfatuledickson <lfatuledickson@mail.valenciacollege.edu>
2022-10-01 16:01:28 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// Coinflip simulator
+
+let userChoice;
+let randomNumber;
+let computerChoice;
+
+userChoice = prompt("This is a coinflip challenge: Please enter \"1\" for \"Heads\" or \"0\" for \"Tails\".");
+
+if (userChoice == 0 || userChoice == 1) {
+    computerChoice = 1 - userChoice;
+
+randomNumber = Math.floor(Math.random() * 2);
+
+if (randomNumber == userChoice) {
+    alert("Congratulations, you have won! Luck is on your side!");
+}
+else if (randomNumber == computerChoice) {
+    alert("Too bad, the coin sided with the computer this time...");
+}
+}
+else {
+    alert("I said 0 or 1! Don't give me anything else you clown!");
+}
+
+// Age confirmation
+
+let birthYear = prompt("We need to confirm that you are 21 years or older to drink. Please enter your age below.");
+
+if (birthYear < 21) {
+       alert("We are sorry. You are not old enough for drinking.");
+   }
+else if (birthYear >= 21) {
+       alert("Welcome to AleCool. Please drink responsibly.");
+   }
